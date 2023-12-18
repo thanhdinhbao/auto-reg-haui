441426c92a17b6b2d70668c2af468c56b17fc4fe
Thanh Dinh <thanhdinhbao@outlook.com.vn>
2023-12-18 17:42:39 +0700

    change vietnamese

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
     // Hàm JavaScript sẽ được gọi khi nhấn nút submit
     function executeFunction() {
-    var dayValue = parseInt(document.getElementById('dropdown1').value,10);
-    var timeValue = parseInt(document.getElementById('dropdown2').value,10);
+    var dayValue = parseInt(document.getElementById('dayDropdown').value,10);
+    var timeValue = parseInt(document.getElementById('timeDropdown').value,10);
       console.log('Selected day:', dayValue);
       console.log('Selected time:', timeValue);
   
@@ -21,49 +21,49 @@ document.addEventListener('DOMContentLoaded', function () {
     switch (dayValue) {
       case 1:
         day = 2;
-        console.log('Chosen day: Monday');
+        console.log('Đã chọn: Thứ Hai');
         break;
       case 2:
       day = 3;
-        console.log('Chosen day: Tuesday');
+        console.log('Đã chọn:  Thứ Ba');
         break;
       case 3:
       day = 4;
-        console.log('Chosen day: Wednesday');
+        console.log('Đã chọn:  Thứ Tư');
         break;
       case 4:
       day = 5;
-        console.log('Chosen day: Thursday');
+        console.log('Đã chọn:  Thứ Năm');
         break;
       case 5:
       day = 6;
-        console.log('Chosen day: Friday');
+        console.log('Đã chọn:  Thứ Sáu');
         break;
       case 6:
       day = 7;
-        console.log('Chosen day: Saturday');
+        console.log('Đã chọn:  Thứ Bảy');
         break;
       case 7:
       day = 8;
-        console.log('Chosen day: Sunday');
+        console.log('Đã chọn:  Chủ Nhật');
         break;
       default:
         // Trường hợp mặc định (nếu có)
-        console.log('Invalid day');
+        console.log('Chọn lại ngày đi bạn!');
     }
 
     switch (timeValue) {
       case 1:
         time = 1;
-        console.log('Chọn buổi sáng!');
+        console.log('Đã chọn buổi sáng!');
         break;
       case 2:
       time = 2;
-        console.log('Chọn buổi chiều!');
+        console.log('Đã chọn buổi chiều!');
         break;
       case 3:
       time = 3;
-        console.log('Chọn buổi tối!');
+        console.log('Đã chọn buổi tối!');
         break;
       default:
         // Trường hợp mặc định (nếu có)
